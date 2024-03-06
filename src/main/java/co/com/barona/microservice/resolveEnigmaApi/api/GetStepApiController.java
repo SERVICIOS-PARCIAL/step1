686286157847903105b8d6685b67c876e545c070
barona13 <baronaa13@hotmail.com>
2024-03-05 20:18:43 -0500

    first commit

---
+++ src/main/java/co/com/barona/microservice/resolveEnigmaApi/api/GetStepApiController.java
@@ -0,0 +1,58 @@
+package co.com.barona.microservice.resolveEnigmaApi.api;
+
+import co.com.barona.microservice.resolveEnigmaApi.model.GetEnigmaRequest;
+import co.com.barona.microservice.resolveEnigmaApi.model.GetEnigmaStepResponse;
+import co.com.barona.microservice.resolveEnigmaApi.model.Header;
+import co.com.barona.microservice.resolveEnigmaApi.model.JsonApiBodyRequest;
+import co.com.barona.microservice.resolveEnigmaApi.model.JsonApiBodyResponseSuccess;
+import io.swagger.annotations.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+import javax.validation.Valid;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-02-27T21:16:02.967-05:00[America/Bogota]")
+@Controller
+public class GetStepApiController implements GetStepApi {
+
+public ResponseEntity<JsonApiBodyResponseSuccess> getStep(@ApiParam(value = "request body get enigma Step", required = true) @Valid @RequestBody JsonApiBodyRequest body) {
+    	
+    	
+    	/*String accept = request.getHeader("Accept");
+    	enigma.setAnswer("Step1: Open the regrigerator");
+    	enigmas.add(enigma);
+    	
+    	response.setData(enigmas);
+    	listResponse.add(response);
+    	
+    	return new ResponseEntity<>(listResponse, HttpStatus.NOT_IMPLEMENTED);*/
+    	
+    	GetEnigmaRequest enigmaRequest = body.getData().get(0);
+        Header header = enigmaRequest.getHeader();
+        String id = header.getId();
+        String type = header.getType();
+        String enigma = enigmaRequest.getEnigma();
+
+        String solution = answerEnigma(enigma);
+
+        GetEnigmaStepResponse response = new GetEnigmaStepResponse();
+        response.setId(id);
+        response.setType(type);
+        response.setSolution(solution);
+
+        JsonApiBodyResponseSuccess responseBody = new JsonApiBodyResponseSuccess();
+        responseBody.addDataItem(response);
+
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    	
+    }
+
+    private String answerEnigma(String enigmaQuestion) {
+        return "Step1: Open the regrigerator.";
+    }
+
+}
